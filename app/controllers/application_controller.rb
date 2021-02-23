6b35656ba87ebcdb5a1d10807ec3dffcee607ea4
yurika-git <yurika_imanishi@icloud.com>
2021-02-23 14:14:06 +0900

    新規登録機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,19 @@
 class ApplicationController < ActionController::Base
    before_action :basic_auth
+  #  before_action :authenticate_user!
+   before_action :configure_permitted_parameters, if: :devise_controller?
 
+   protected
+   def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :surname, :name, :surname_kana, :name_kana, :birthday])
+   end
 
   private
 
    def basic_auth
      authenticate_or_request_with_http_basic do |username, password|
-      username == 'user' && password == '0000'
-    end
+     username == 'user' && password == '0000'
+   end
   end
 end
+ 
