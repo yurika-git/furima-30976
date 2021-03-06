4369cff91808721367ee039d43d5955ef66f9aa1
yurika-git <yurika_imanishi@icloud.com>
2021-03-06 15:28:51 +0900

    レビュー後訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,23 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :email, presence: true
-  #validates :encrypted_password, presence: true, length: { minimum: 6 }       # 「６文字以上」
-  validates :surname, presence: true
-  validates :name, presence: true
-  validates :surname_kana, presence: true
-  validates :name_kana, presence: true
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :email
+  end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :encrypted_password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
     validates :surname
     validates :name
   end
-
   with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナを使用してください' } do
     validates :surname_kana
     validates :name_kana
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
+
   end
 
   describe '新規登録/ユーザー情報' do
@@ -15,9 +16,10 @@ RSpec.describe User, type: :model do
         expect(@user).to be_valid
       end
       it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
-        @user.password = '000000'
-        @user.password_confirmation = '000000'
+        @user.password = '12345a'
+        @user.password_confirmation = '12345a'
         expect(@user).to be_valid
+
       end
       it 'passwordとpassword_confirmationが半角英数字混合であれば登録できる' do
         @user.password = '12345a'
@@ -32,6 +34,30 @@ RSpec.describe User, type: :model do
       it 'passwordとpassword_confirmationが一致すれば登録できる' do
         @user.password = '12345a'
         @user.password_confirmation = '12345a'
+        expect(@user).to be_valid
+      end
+      it 'nameとsurnameが入力されていれば登録できる' do
+        @user.name = 'あああ'
+        @user.surname = 'あああ'
+        expect(@user).to be_valid
+      end
+      it 'nameとsurnameが全角であれば登録できる' do
+        @user.name = 'あああ'
+        @user.surname = 'あああ'
+        expect(@user).to be_valid
+      end
+      it 'name_kanaとsurname_kanaが入力されていれば登録できる' do
+        @user.name_kana = 'アアア'
+        @user.surname_kana = 'アアア'
+        expect(@user).to be_valid
+      end
+      it 'name_kanaとsurname_kanaが全角カナであれば登録できる' do
+        @user.name_kana = 'アアア'
+        @user.surname_kana = 'アアア'
+        expect(@user).to be_valid
+      end
+      it 'birthdayが存在すれば登録できる' do
+        @user.birthday = '1999-01-01'
         expect(@user).to be_valid
       end
     end
@@ -57,7 +83,7 @@ RSpec.describe User, type: :model do
       it 'emailに＠が無い場合登録できない' do
         @user.email = 'aaaa1111'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email is no @ mark")
+        expect(@user.errors.full_messages).to include("Email is invalid")
 
       end
       it 'passwordが空では登録できない' do
@@ -75,30 +101,59 @@ RSpec.describe User, type: :model do
         @user.password = '000000'
         @user.password_confirmation = '00000'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is no alphabet")
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
       it 'passwordに数字が無い場合登録できない' do
         @user.password = 'aaaaaa'
         @user.password_confirmation = 'aaaaaa'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is no numbers")
+        expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
       end
       it 'passwordが全角の場合登録できない' do
         @user.password = 'ａａａａａａ'
         @user.password_confirmation = 'ａａａａａａ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is Full-width")
+        expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
       end
       it 'passwordが存在してもpassword_confirmationが空では登録できない' do
         @user.password_confirmation = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password confirmation can't be blank")
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
       it 'passwordとpassword_confirmationが一致しない場合登録できない' do
         @user.password = '12345a'
         @user.password_confirmation = '67890a'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'nameが存在してもsurnameが空では登録できない' do
+        @user.name = 'あああ'
+        @user.surname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Surname can't be blank", "Surname 全角文字を使用してください")
+      end
+      it 'nameとsurnameは全角でないと登録できない' do
+        @user.name = 'ｱｱｱ'
+        @user.surname = 'ｱｱｱ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Surname 全角文字を使用してください", "Name 全角文字を使用してください")
+      end
+      it 'name_kanaが存在してもsurname_kanaが空では登録できない' do
+        @user.name_kana = 'アアア'
+        @user.surname_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Surname kana can't be blank", "Surname kana 全角カタカナを使用してください")
+      end
+      it 'name_kanaとsurname_kanaは全角カナでないと登録できない' do
+        @user.name_kana = 'あああ'
+        @user.surname_kana = 'あああ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Surname kana 全角カタカナを使用してください", "Name kana 全角カタカナを使用してください")
+      end
+      it 'birthdayが空では登録できない' do
+        @user.birthday = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
       end
     end
   end
