0c0c6079a5ff84226f6107b16815c1647d1d1b58
yurika-git <yurika_imanishi@icloud.com>
2021-03-09 21:18:22 +0900

    テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
+    product         {"商品"}
+    description     {"あああああ"}
+    category_id     {1}
+    condition_id    {1}
+    charge_id       {1}
+    area_id         {1}
+    shipping_id     {1}
+    price           {300}
+    association :user
     
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
