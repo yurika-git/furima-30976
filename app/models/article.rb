e164bd7ae59aa53dad785a7dbf40914b50b83e44
yurika-git <yurika_imanishi@icloud.com>
2021-03-10 20:08:08 +0900

    activehashアソシエーション

--- app/models/article.rb
+++
@@ -1,8 +0,0 @@
-class Article < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :condition
-  belongs_to :charge
-  belongs_to :shipping
-  belongs_to :area
-end
