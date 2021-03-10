f7978cd5aae5649c0bf0929fe88b36a009d932ac
yurika-git <yurika_imanishi@icloud.com>
2021-03-10 19:37:42 +0900

    active_hashテストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -73,7 +73,11 @@ RSpec.describe Item, type: :model do
           @item.valid?
           expect(@item.errors.full_messages).to include("Price is not a number")
         end
-        
+        it 'active_hashの値が1であれば保存しない' do
+          @item.category_id = 1
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Category must be other than 1")
+        end
       end
     end
 end
