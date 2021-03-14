16bd2685ac18156be38f6d9e5936ab10e7ef2edd
yurika-git <yurika_imanishi@icloud.com>
2021-03-14 15:13:02 +0900

    出品商品一覧表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
-    # @item = Item.find(item_params[:user_id])
+    @items = Item.all
   end
 
   def new
