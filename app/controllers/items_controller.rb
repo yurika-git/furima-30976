4f1437fdc070aaa100ca93a83c3186cf8bd27e7a
yurika-git <yurika_imanishi@icloud.com>
2021-03-19 20:25:57 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_items, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_items, only: [:show, :edit, :update, :destroy]
   before_action :contributor_confirmation, only: [:edit, :update]
   
 
@@ -36,6 +36,13 @@ class ItemsController < ApplicationController
      render :edit
     end
   end
+
+  def destroy
+    if @item.destroy
+    redirect_to root_path
+    end
+  end
+
 
 
   private
